48984af53cfda676831dc2318116e14d30d4965e
Steve Carter <Scarter31191@gmail.com>
2020-07-19 09:50:20 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+    attr_accessor :name
+
+    def initilize(name)
+        @name = name
+    end
+    
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+
+    attr_accessor :name
+
+    def initilize(name)
+        @name = name
+    end
+    
+end
